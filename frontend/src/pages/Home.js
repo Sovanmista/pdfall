1ff83f36a9c421ac22a3ebebb3daea43b364738f
Susovan Mishra <143598676+Sovanmista@users.noreply.github.com>
2024-12-01 03:33:30 +0530

    Add files via upload

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Header from '../component/Header';
+import Sections from '../component/Sections';
+import Footer from '../component/Footer';
+import Credits from '../component/Credits';
+function Home() {
+  return (
+    <div>
+       <Header/>
+      <Sections/>
+      <Footer/>
+      <Credits/>
+    </div>
+  )
+}
+
+export default Home
